8416918cd79fca4e1a59efc30fd84001b846d171
S SANTHOSH SRIRAM <santhoshram3210@gmail.com>
2023-12-26 14:51:31 +0530

    home update

--- src/routers/routersIndex.tsx
+++ src/routers/routersIndex.tsx
@@ -1,23 +1,32 @@
 import { useRoutes } from "react-router-dom";
 import ProductEditIndex from "../components/ishop/products/collections/productEdit/productEditIndex";
+import HomeIndex from "../components/ishop/home/homeIndex";
 
 
 
 export default function RouterIndex() {
 
-    interface Route {
-        path: string;
-        element: React.ReactElement;
-      }
+  interface Route {
+    path: string;
+    element: React.ReactElement;
+  }
 
-    const routes :Route[] = [
-      {  path: '/productsedit',
-        element: <ProductEditIndex/>,}
-    ]
+  const routes: Route[] = [
+    {
+
+      path: '/productsedit',
+      element: <ProductEditIndex />,
+
+    },
+    {
+      path: '/',
+      element: <HomeIndex />,
+    }
+  ]
 
 
-    const matchedRoute = useRoutes(routes);
-   
+  const matchedRoute = useRoutes(routes);
+
 
   return (
     <div>{matchedRoute}</div>
