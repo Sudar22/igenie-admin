ca19bf978a65f4754ccb580b40fe85ac5a3765fb
Sudar22 <cndemso25@outlook.com>
2024-02-20 15:03:38 +0530

    add button

--- src/pages/products/productPage.tsx
+++ src/pages/products/productPage.tsx
@@ -1,7 +1,7 @@
-import { Helmet } from 'react-helmet-async';
-import { filter } from 'lodash';
-import { sentenceCase } from 'change-case';
-import { useState, ChangeEvent } from 'react';
+import { Helmet } from "react-helmet-async";
+import { filter } from "lodash";
+import { sentenceCase } from "change-case";
+import { useState, ChangeEvent } from "react";
 import {
   Card,
   Table,
@@ -21,13 +21,16 @@ import {
   TableContainer,
   TablePagination,
   ListItemButton,
-} from '@mui/material';
-import { ProductListHead, ProductListToolbar } from '../../sections/@Dashboard/products';
-import Label from '../../components/label';
-import Iconify from '../../components/iconify';
-import Scrollbar from '../../components/scrollbar';
-import USERLIST from '../../_mock/user';
-import { Link } from 'react-router-dom';
+} from "@mui/material";
+import {
+  ProductListHead,
+  ProductListToolbar,
+} from "../../sections/@Dashboard/products";
+import Label from "../../components/label";
+import Iconify from "../../components/iconify";
+import Scrollbar from "../../components/scrollbar";
+import USERLIST from "../../_mock/user";
+import { Link } from "react-router-dom";
 
 interface User {
   id: string;
@@ -38,7 +41,6 @@ interface User {
   avatarUrl: string;
   isVerified: boolean;
   [key: string]: any; // Allow any string as an index
-
 }
 
 interface HeadCell {
@@ -48,12 +50,12 @@ interface HeadCell {
 }
 
 const TABLE_HEAD: HeadCell[] = [
-  { id: 'product', label: 'Product', alignRight: false },
-  { id: 'vendor', label: 'Vendor', alignRight: false },
-  { id: 'category', label: 'Category', alignRight: false },
-  { id: 'isVerified', label: 'Verified', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
-  { id: 'action', label: 'Action', alignRight: false },
+  { id: "product", label: "Product", alignRight: false },
+  { id: "vendor", label: "Vendor", alignRight: false },
+  { id: "category", label: "Category", alignRight: false },
+  { id: "isVerified", label: "Verified", alignRight: false },
+  { id: "status", label: "Status", alignRight: false },
+  { id: "action", label: "Action", alignRight: false },
 ];
 
 function descendingComparator(a: User, b: User, orderBy: string) {
@@ -66,8 +68,8 @@ function descendingComparator(a: User, b: User, orderBy: string) {
   return 0;
 }
 
-function getComparator(order: 'asc' | 'desc', orderBy: string) {
-  return order === 'desc'
+function getComparator(order: "asc" | "desc", orderBy: string) {
+  return order === "desc"
     ? (a: User, b: User) => descendingComparator(a, b, orderBy)
     : (a: User, b: User) => -descendingComparator(a, b, orderBy);
 }
@@ -85,32 +87,43 @@ function getComparator(order: 'asc' | 'desc', orderBy: string) {
 //   return stabilizedThis.map((el) => el[0]);
 // }
 
-function applySortFilter(array: User[], comparator: (a: User, b: User) => number, query: string) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-      const order = comparator(a[0], b[0]); // Compare only the User objects, not the entire tuple
-      if (order !== 0) return order;
-      return a[1] - b[1];
-    });
-    if (query) {
-      return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-    }
-    return stabilizedThis.map((el) => el[0]);
+function applySortFilter(
+  array: User[],
+  comparator: (a: User, b: User) => number,
+  query: string
+) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]); // Compare only the User objects, not the entire tuple
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(
+      array,
+      (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
   }
-  
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface Button {
+  label: string;
+  name: string;
+}
 
 export default function ProductPage() {
   const [open, setOpen] = useState<null | HTMLElement>(null);
 
   const [page, setPage] = useState(0);
 
-  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
 
   const [selected, setSelected] = useState<string[]>([]);
 
-  const [orderBy, setOrderBy] = useState('name');
+  const [orderBy, setOrderBy] = useState("name");
 
-  const [filterName, setFilterName] = useState('');
+  const [filterName, setFilterName] = useState("");
 
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
@@ -122,9 +135,12 @@ export default function ProductPage() {
     setOpen(null);
   };
 
-  const handleRequestSort = (event: React.MouseEvent<unknown>, property: string) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: string
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -139,7 +155,7 @@ export default function ProductPage() {
 
   const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
     const selectedIndex = selected.indexOf(name);
-    let newSelected  = [] as any;
+    let newSelected = [] as any;
     if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected, name);
     } else if (selectedIndex === 0) {
@@ -147,16 +163,24 @@ export default function ProductPage() {
     } else if (selectedIndex === selected.length - 1) {
       newSelected = newSelected.concat(selected.slice(0, -1));
     } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
     }
     setSelected(newSelected);
   };
 
-  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setPage(0);
     setRowsPerPage(parseInt(event.target.value, 10));
   };
@@ -166,11 +190,37 @@ export default function ProductPage() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
 
-  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
+  const filteredUsers = applySortFilter(
+    USERLIST,
+    getComparator(order, orderBy),
+    filterName
+  );
 
   const isNotFound = !filteredUsers.length && !!filterName;
+
+  const [buttons, setButtons] = useState<Button[]>([
+    { label: "Button 1", name: "button1" },
+  ]);
+
+  const handleAddButton = () => {
+    setButtons([
+      ...buttons,
+      { label: "New Button", name: `button${buttons.length + 1}` },
+    ]);
+  };
+
+  const handleEditButtonName = (
+    event: React.SyntheticEvent<HTMLDivElement>,
+    index: number
+  ) => {
+    let { innerText } = event.currentTarget;
+    let editedButtons = [...buttons];
+    editedButtons[index].name = innerText;
+    setButtons(editedButtons);
+  };
 
   return (
     <>
@@ -179,64 +229,82 @@ export default function ProductPage() {
       </Helmet>
 
       <Container>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Typography variant="h4" gutterBottom>
+            Products
+          </Typography>
 
-
-              <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                  <Typography variant="h4" gutterBottom>
-                      Products
-                  </Typography>
-
-                  <Stack direction="row" spacing={1.2} flexWrap="wrap-reverse">
-                      {/* <Button variant="contained" color="secondary" size="medium">
+          <Stack direction="row" spacing={1.2} flexWrap="wrap-reverse">
+            {/* <Button variant="contained" color="secondary" size="medium">
                           Export
                       </Button>
                       <Button variant="contained" color="secondary" size="medium">
                           import
                       </Button> */}
 
+            <Button
+              variant="contained"
+              component={Link}
+              to="addproduct"
+              size="medium"
+              startIcon={<Iconify icon="eva:plus-fill" />}
+            >
+              add Product
+            </Button>
 
-                      <Button variant="contained" component={Link} to="addproduct" size="medium" startIcon={<Iconify icon="eva:plus-fill" />}>
-                          add Product
-                      </Button> 
-
-                      {/* <ListItemButton to='products/addproduct' relative="/dashboard" >
+            {/* <ListItemButton to='products/addproduct' relative="/dashboard" >
                           add Product
                       </ListItemButton>  */}
 
-                      {/* <Link to='products/addproduct'>
+            {/* <Link to='products/addproduct'>
                       <Button variant="contained" size="medium" startIcon={<Iconify icon="eva:plus-fill" />} onClick={() => { }}>
                           add Product
                       </Button>
                       </Link> */}
-                  </Stack>
-
-                 
+          </Stack>
         </Stack>
 
+        <Stack>
+          <Card>
+            <ProductListToolbar
+              numSelected={selected.length}
+              filterName={filterName}
+              onFilterName={handleFilterByName}
+            />
+          </Card>
+        </Stack>
 
-             <Stack >
-             <Card>
-                  <ProductListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
-              </Card>
-             </Stack>
-
-
-        <Card >
-
+        <Card>
           <Scrollbar>
-
             <Card>
               <Stack direction="row" p={1}>
-                <Button size="small" >ALL</Button>
+                <Button size="small">ALL</Button>
                 <Button size="small">Active</Button>
                 <Button size="small">Draft</Button>
-                <Button size="small">+</Button>
+                {buttons.map((button, index) => (
+                  <div>
+                    <div
+                      key={index}
+                      contentEditable={true}
+                      onInput={(event) => handleEditButtonName(event, index)}
+                    >
+                      <Button>{button.name}</Button>
+                    </div>
+                    {/* <button onClick={() => handleDeleteButton(index)}>Delete Button</button> */}
+                  </div>
+                ))}
+                <Button size="small" onClick={handleAddButton}>
+                  +
+                </Button>
               </Stack>
-             
             </Card>
             <TableContainer sx={{ minWidth: 800 }}>
               <Table>
-
                 <ProductListHead
                   order={order}
                   orderBy={orderBy}
@@ -247,47 +315,80 @@ export default function ProductPage() {
                   onSelectAllClick={handleSelectAllClick}
                 />
                 <TableBody>
-                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                  {filteredUsers
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      const {
+                        id,
+                        name,
+                        category,
+                        status,
+                        company,
+                        avatarUrl,
+                        isVerified,
+                      } = row;
 
+                      const selectedUser = selected.indexOf(name) !== -1;
 
+                      return (
+                        <TableRow
+                          hover
+                          key={id}
+                          tabIndex={-1}
+                          role="checkbox"
+                          selected={selectedUser}
+                        >
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              checked={selectedUser}
+                              size="small"
+                              onChange={(event) => handleClick(event, name)}
+                            />
+                          </TableCell>
 
-                    const { id, name, category, status, company, avatarUrl, isVerified }  = row;
+                          <TableCell component="th" scope="row" padding="none">
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar alt={name} src={avatarUrl} />
+                              <Typography variant="subtitle2" noWrap>
+                                {name}
+                              </Typography>
+                            </Stack>
+                          </TableCell>
 
-                    const selectedUser = selected.indexOf(name) !== -1;
+                          <TableCell align="left">{company}</TableCell>
 
-                    return (
-                      <TableRow hover key={id} tabIndex={-1} role="checkbox" selected={selectedUser}>
-                        <TableCell padding="checkbox">
-                          <Checkbox checked={selectedUser}  size="small" onChange={(event) => handleClick(event, name)} />
-                        </TableCell>
+                          <TableCell align="left">{category}</TableCell>
 
-                        <TableCell component="th" scope="row" padding="none">
-                          <Stack direction="row" alignItems="center" spacing={2}>
-                            <Avatar alt={name} src={avatarUrl} />
-                            <Typography variant="subtitle2" noWrap>
-                              {name}
-                            </Typography>
-                          </Stack>
-                        </TableCell>
+                          <TableCell align="left">
+                            {isVerified ? "Yes" : "No"}
+                          </TableCell>
 
-                        <TableCell align="left">{company}</TableCell>
+                          <TableCell align="left">
+                            <Label
+                              color={
+                                (status === "pending" && "error") || "success"
+                              }
+                            >
+                              {sentenceCase(status)}
+                            </Label>
+                          </TableCell>
 
-                        <TableCell align="left">{category}</TableCell>
-
-                        <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
-
-                        <TableCell align="left">
-                          <Label color={(status === 'pending' && 'error') || 'success'}>{sentenceCase(status)}</Label>
-                        </TableCell>
-
-                        <TableCell align="right">
-                          <IconButton size="large" color="inherit" onClick={handleOpenMenu}>
-                            <Iconify icon={'eva:more-vertical-fill'} />
-                          </IconButton>
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
+                          <TableCell align="right">
+                            <IconButton
+                              size="large"
+                              color="inherit"
+                              onClick={handleOpenMenu}
+                            >
+                              <Iconify icon={"eva:more-vertical-fill"} />
+                            </IconButton>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
 
                   {emptyRows > 0 && (
                     <TableRow style={{ height: 53 * emptyRows }}>
@@ -302,7 +403,7 @@ export default function ProductPage() {
                       <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
                         <Paper
                           sx={{
-                            textAlign: 'center',
+                            textAlign: "center",
                           }}
                         >
                           <Typography variant="h6" paragraph>
@@ -312,7 +413,8 @@ export default function ProductPage() {
                           <Typography variant="body2">
                             No results found for &nbsp;
                             <strong>&quot;{filterName}&quot;</strong>.
-                            <br /> Try checking for typos or using complete words.
+                            <br /> Try checking for typos or using complete
+                            words.
                           </Typography>
                         </Paper>
                       </TableCell>
@@ -339,27 +441,27 @@ export default function ProductPage() {
         open={Boolean(open)}
         anchorEl={open}
         onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         PaperProps={{
           sx: {
             p: 1,
             width: 140,
-            '& .MuiMenuItem-root': {
+            "& .MuiMenuItem-root": {
               px: 1,
-              typography: 'body2',
+              typography: "body2",
               borderRadius: 0.75,
             },
           },
         }}
       >
         <MenuItem>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
+          <Iconify icon={"eva:edit-fill"} sx={{ mr: 2 }} />
           Edit
         </MenuItem>
 
-        <MenuItem sx={{ color: 'error.main' }}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
+        <MenuItem sx={{ color: "error.main" }}>
+          <Iconify icon={"eva:trash-2-outline"} sx={{ mr: 2 }} />
           Delete
         </MenuItem>
       </Popover>
--- src/sections/@Dashboard/products/@product/Variants.tsx
+++ src/sections/@Dashboard/products/@product/Variants.tsx
@@ -1,178 +1,150 @@
-import React, { useState } from 'react';
-import { styled, alpha, Theme } from '@mui/material/styles';
-import { Button, Card, Container, Divider, IconButton, OutlinedInput, Stack, Tooltip, Typography } from '@mui/material';
-import Iconify from '../../../../components/iconify';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { styled, alpha, Theme } from "@mui/material/styles";
+import {
+  Button,
+  Card,
+  Container,
+  Divider,
+  IconButton,
+  OutlinedInput,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import Iconify from "../../../../components/iconify";
+import { Link } from "react-router-dom";
 
+const StyledRoot = styled("div")(({ theme }: { theme: Theme }) => ({
+  width: 700,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  padding: theme.spacing(0, 0, 0, 0),
 
-const StyledRoot = styled('div')(({ theme }: { theme: Theme }) => ({
-
-    width: 700,
-    display: 'flex',
-    flexDirection: "column",
-    justifyContent: 'space-between',
-    padding: theme.spacing(0, 0, 0, 0),
-
-    margin: theme.spacing(2.5, 0, 0, 0),
-
+  margin: theme.spacing(2.5, 0, 0, 0),
 }));
-const StyledInputBox = styled('div')(({ theme }: { theme: Theme }) => ({
-    display: 'flex',
-    flexDirection: "column",
-    justifyContent: 'space-between',
-    padding: theme.spacing(1, 4, 1, 4),
+const StyledInputBox = styled("div")(({ theme }: { theme: Theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  padding: theme.spacing(1, 4, 1, 4),
 
-    margin: theme.spacing(0, 0, 0, 0),
-
+  margin: theme.spacing(0, 0, 0, 0),
 }));
-const StyledHead = styled('div')(({ theme }: { theme: Theme }) => ({
-    display: 'flex',
-    flexDirection: "row",
-    justifyContent: 'space-between',
-    padding: theme.spacing(2, 0, 0, 1.5),
+const StyledHead = styled("div")(({ theme }: { theme: Theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  padding: theme.spacing(2, 0, 0, 1.5),
 
-    margin: theme.spacing(0, 0, 1, 0),
-
+  margin: theme.spacing(0, 0, 1, 0),
 }));
-const StyledOption = styled('div')(({ theme }: { theme: Theme }) => ({
-    display: 'flex',
-    flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: theme.spacing(0, 0, 0, 0),
+const StyledOption = styled("div")(({ theme }: { theme: Theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: theme.spacing(0, 0, 0, 0),
 
-    margin: theme.spacing(0, 0, 1, 0),
-
+  margin: theme.spacing(0, 0, 1, 0),
 }));
 
 const StyledSearch = styled(OutlinedInput)(({ theme }: { theme: Theme }) => ({
-    width: 580,
-    height: 40,
-    margin: theme.spacing(0, 0, 2, 0),
-    transition: theme.transitions.create(['box-shadow', 'width'], {
-        easing: theme.transitions.easing.easeInOut,
-        duration: theme.transitions.duration.shorter,
-    }),
-    '&.Mui-focused': {
-        //   width: 350,
-        boxShadow: theme.customShadows.z8,
-    },
-    '& fieldset': {
-        borderWidth: `1px !important`,
-        borderColor: `${alpha(theme.palette.grey[500], 0.32)} !important`,
-    },
+  width: 580,
+  height: 40,
+  margin: theme.spacing(0, 0, 2, 0),
+  transition: theme.transitions.create(["box-shadow", "width"], {
+    easing: theme.transitions.easing.easeInOut,
+    duration: theme.transitions.duration.shorter,
+  }),
+  "&.Mui-focused": {
+    //   width: 350,
+    boxShadow: theme.customShadows.z8,
+  },
+  "& fieldset": {
+    borderWidth: `1px !important`,
+    borderColor: `${alpha(theme.palette.grey[500], 0.32)} !important`,
+  },
 }));
 
-
-
-
 export function Option() {
+  return (
+    <StyledInputBox>
+      <StyledOption>
+        <Stack>
+          <Typography variant="subtitle1" component="h6" fontSize="24">
+            Option name
+          </Typography>
+          <StyledSearch />
+        </Stack>
 
-    return (
-        <StyledInputBox>
-            <StyledOption>
-                <Stack>
-                    <Typography variant='subtitle1' component='h6' fontSize='24'>
-                        Option name
-                    </Typography>
-                    <StyledSearch />
-                </Stack>
+        <Stack>
+          <Tooltip title="delete">
+            <IconButton>
+              <Iconify
+                icon="material-symbols:delete-outline"
+                sx={{ width: 25, height: 25 }}
+              />
+            </IconButton>
+          </Tooltip>
+        </Stack>
+      </StyledOption>
+      <StyledOption>
+        <Stack>
+          <Typography variant="subtitle1" component="h6" fontSize="24">
+            Option values
+          </Typography>
+          <StyledSearch />
+        </Stack>
+      </StyledOption>
 
-
-                <Stack>
-                    <Tooltip title="delete">
-                        <IconButton  >
-                            <Iconify icon="material-symbols:delete-outline" sx={{ width: 25, height: 25 }} />
-                        </IconButton>
-
-                    </Tooltip>
-                </Stack>
-
-
-            </StyledOption>
-            <StyledOption>
-                <Stack>
-                    <Typography variant='subtitle1' component='h6' fontSize='24'>
-                        Option values
-                    </Typography>
-                    <StyledSearch />
-                </Stack>
-
-
-
-            </StyledOption>
-
-            <Stack >
-                <Button>
-                    done
-                </Button>
-            </Stack>
-            <Divider/>
-        </StyledInputBox>
-
-    )
-
-};
-
+      <Stack>
+        <Button>done</Button>
+      </Stack>
+      <Divider />
+    </StyledInputBox>
+  );
+}
 
 export default function Variants() {
+  // const [add,setAdd]=useState<Boolean>(false);
+  
 
-    // const [add,setAdd]=useState<Boolean>(false);
+  return (
+    <StyledRoot>
+      <Card>
+        <StyledHead>
+          <Typography
+            variant="subtitle1"
+            component="h6"
+            fontSize="24"
+            fontWeight="bold"
+          >
+            Variant
+          </Typography>
+        </StyledHead>
 
+        <Stack direction="row" pl={2} justifyContent="center">
+          <Tooltip title="add">
+            <IconButton>
+              <Iconify icon="carbon:add" sx={{ width: 25, height: 25 }} />
+            </IconButton>
+          </Tooltip>
 
+          <Button> add options like size or color </Button>
+        </Stack>
 
-    return (
-        <StyledRoot>
+        <Option />
+        <Option />
 
+        <Divider />
 
+        <Stack>
+          <Button>add anthor option </Button>
+        </Stack>
+      </Card>
+    </StyledRoot>
 
-            <Card>
-                <StyledHead>
-                    <Typography variant='subtitle1' component='h6' fontSize='24' fontWeight='bold'>
-                        Variant
-                    </Typography>
-
-
-                </StyledHead>
-
-
-                <Stack direction="row" pl={2} justifyContent="center">
-                    <Tooltip title="add">
-                        <IconButton  >
-                            <Iconify icon="carbon:add" sx={{ width: 25, height: 25 }} />
-                        </IconButton>
-
-                    </Tooltip>
-
-                    <Button> add  options like size or color </Button>
-
-
-                </Stack>
-
-
-
-
-                <Option />
-                <Option />
-
-
-                <Divider />
-
-
-
-                <Stack>
-                    <Button>add anthor option </Button>
-
-                </Stack>
-            </Card>
-
-
-
-
-
-
-        </StyledRoot>
-
-        // <div>hello</div>
-    )
+    // <div>hello</div>
+  );
 }
